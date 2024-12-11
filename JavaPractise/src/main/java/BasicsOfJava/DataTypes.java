cc2ee482f90a6841233646bcae0c6e1fd0d2fd12
vsharma1129 <vs199429@gmail.com>
2024-12-11 07:57:17 +0530

    Testing commit 1

---
+++ JavaPractise/src/main/java/BasicsOfJava/DataTypes.java
@@ -0,0 +1,22 @@
+package BasicsOfJava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean a=true;
+		boolean b=false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		b=true;
+		a=false; //updating values 
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
---
+++ JavaPractise/src/main/java/Primitive/Boolean.java
@@ -0,0 +1,23 @@
+package Primitive;
+
+public class Boolean {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean a=true;
+		boolean b=false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		b=true;
+		a=false; //updating values 
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
