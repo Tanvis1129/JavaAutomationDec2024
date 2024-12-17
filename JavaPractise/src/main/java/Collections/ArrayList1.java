feb8772bd6eea1c7c33ae3ee562e099ea45d6a17
Tanvis1129 <tanvis1129@gmail.com>
2024-12-17 08:50:27 +0530

    Update ArrayList1.java

--- JavaPractise/src/main/java/Collections/ArrayList1.java
+++ JavaPractise/src/main/java/Collections/ArrayList1.java
@@ -8,8 +8,7 @@ import java.util.List;
 public class ArrayList1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		List <String> fruits = new ArrayList<>();
 		
 		fruits.add("Apple");
